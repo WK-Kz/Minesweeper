36d9eb2c950416cdb8b2c66a6deab403b803c5f0
WK-M <molaskendall@gmail.com>
2018-11-20 22:23:40 -0500

    Border collision

--- Minesweeper Game/kendall.asm
+++ Minesweeper Game/kendall.asm
@@ -1,10 +1,14 @@
 ; Kendall Molas
 ; CSc 211
+
+
 .MODEL SMALL
 .STACK 100h 
 .CODE
 assume cs:cseg, ds:cseg
 cseg segment
+
+JUMPS
 
 START:
     ; Push Video into ES
@@ -31,7 +35,7 @@ START:
         mov BYTE PTR es:[di], 205 
         add di, 2 
         cmp di, 60
-		je fix_di_l1
+        je fix_di_l1
         loop l1
 
     fix_di_l1: ; Fix di 
@@ -96,6 +100,7 @@ START:
         xor ax,ax
         mov ax, 1660
 
+    ; The ascii added to this row is done every four bites.
     bot_rows:
         mov BYTE PTR es:[di], 193
         add di, 4
@@ -103,6 +108,7 @@ START:
         jnb fix_di_rows
         loop bot_rows
 
+    ; Clear Registers
     fix_di_rows:
         xor di, di
         mov di, 320
@@ -113,11 +119,13 @@ START:
     begin_row_di:
         add di, 2
         jmp build_row
-
+    
+    ; Used in loop
     repeat_cycle_row:
         mov BYTE PTR es:[di], 196 ; Place last - in board
         add di, 264
         add ax, 320
+        mov ah, 02h
         cmp ax, 1658
         jnb fix_di_cols
         jmp build_row
@@ -148,7 +156,7 @@ START:
         add di, 264
         add ax, 320
         cmp di, 1498
-        jnb enable_cursor_movement
+        jnb CLEAN
         jmp build_columns
 
     ; Main column building
@@ -158,6 +166,16 @@ START:
         cmp di, ax
         jnb repeat_cycle_column
         loop build_columns
+
+    CLEAN:
+        xor ax, ax
+        xor dx, dx
+        jmp enable_cursor_movement
+
+    BLOCK_CURSOR:
+        mov ax, 01h
+        mov cx, 0007h
+        int 10h
 
     enable_cursor_movement:
         mov ah, 02h
@@ -184,32 +202,48 @@ START:
         cmp al, 'a'
         je LEFT_KEY
 
+        cmp al, 'r'
+        je START
+
+        cmp al, 39
+        je SCORE
+
         cmp ah, 42h ; Exit key
         je DONE
 
-        cmp 
-
         jne KEYPRESS
+
+    SCORE:
+        msg db 'Use WASD to move up, left, down, and right', 0
+        
     
     DOWN_KEY:
+        cmp dh, 9 ; Restrict to bounding of box
+        jge KEYPRESS
         mov ah, 02h
         add dh, 2
         int 10h
         jmp KEYPRESS
 
     UP_KEY:
+        cmp dh, 1
+        jle KEYPRESS
         mov ah, 02h
         sub dh, 2
         int 10h
         jmp KEYPRESS
 
     RIGHT_KEY:
+        cmp dl, 29
+        jge KEYPRESS
         mov ah, 02h
         add dl, 2
         int 10h
         jmp KEYPRESS
 
     LEFT_KEY:
+        cmp dl, 1
+        jle KEYPRESS
         mov ah, 02h
         sub dl, 2
         int 10h
